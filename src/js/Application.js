4398b92a0e3c49197fcaad848bf825177ff8bf5c
ivaylo91 <ipenev91@gmail.com>
2023-01-02 13:59:29 +0200

    Finished

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,29 +2,89 @@ import EventEmitter from "eventemitter3";
 import image from "../images/planet.svg";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.innerHTML = this._render({
-      name: "Placeholder",
-      terrain: "placeholder",
-      population: 0,
-    });
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.innerHTML = this._render({
+            name: "Placeholder",
+            terrain: "placeholder",
+            population: 0,
+        });
+        document.body.querySelector(".main").appendChild(box);
 
-    document.body.querySelector(".main").appendChild(box);
+        this.emit(Application.events.READY);
+    }
 
-    this.emit(Application.events.READY);
-  }
+    async _load() {
+        let url = "https://swapi.boom.dev/api/planets";
+        let res = await fetch(url);
+        let planets = await res.next;
+        let next = planets.next;
+        this.arr = [...this.arr, ...planets.results];
+        while (next) {
+            res = await fetch(next);
+            planets = await res.json();
+            next = planets.next;
+            this.arr = [...this.arr, ...planets.results];
+        }
+        this.arr.forEach((planet) => {
+            this._create(planet.name, planet.terrain, planet.population);
+        });
+        this._stopLoading();
+    }
 
-  _render({ name, terrain, population }) {
+    _create(name, terrain, population) {
+        const planet = document.createElement('div');
+        planet.classList.add('box');
+        planet.innerHTML = this._render({
+            name: name,
+            terrain: terrain,
+            population: population,
+        })
+        document.body.querySelector('.main').appendChild(planet);
+    }
+
+    async _startLoading() {
+        await this._load();
+    }
+
+    _stopLoading() {
+        this._loading.style.display = 'none';
+    }
+
+    _render({name, terrain, population}) {
+        return `
+<article class="media">
+  <div class="media-left">
+    <figure class="image is-64x64">
+      <img src="${image}" alt="planet">
+    </figure>
+  </div>
+  <div class="media-content">
+    <div class="content">
+    <h4>${name}</h4>
+      <p>
+        <span class="tag">${terrain}</span> <span class="tag">${population}</span>
+        <br>
+      </p>
+    </div>
+  </div>
+</article>
+    `;
+    }
+}
+
+
+_render({name, terrain, population})
+{
     return `
 <article class="media">
   <div class="media-left">
@@ -43,5 +103,5 @@ export default class Application extends EventEmitter {
   </div>
 </article>
     `;
-  }
 }
+
